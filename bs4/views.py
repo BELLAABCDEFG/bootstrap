0d83a4df55a3c356f02ad3325166e5efb80ee582
llj <2642068463@qq.com>
2019-07-18 20:31:41 +0800

    2019/7.18 8:30

---
+++ bs4/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "bs4/index.html")
+
+def aboutus(request):
+    return render(request, "bs4/aboutus.html")
+
+def contactus(request):
+    return render(request, "bs4/contactus.html")
